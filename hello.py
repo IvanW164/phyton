3c3d1d518afcce30d35ab53b2efdf17f4bb15d75
IvanKratenko <kratenko_ivan@mail.ru>
2022-03-04 01:10:58 +0700

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print('hello world')
+a = 123
+b = 1.23
+s = 'hello world'
+print(a)
+print(b)
+print(s)
+print(f'{a} - {b} - {s}') # интерпаляция
+list = [1,2, 'hello', 123.2] # списик массивы
+print(list)
+
+print('введите a') #вывод данных
+a = input() # ввод данных
+print('введите b')
+b = input()
+print(f'{a} -{b}')
